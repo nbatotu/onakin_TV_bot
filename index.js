5967df94057e5f09c6a1f7c3b0c6d3dda8375df9
nbatotu <nbatotu+ac@gmail.com>
2020-08-09 11:49:09 +0900

    Awesome

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on('message', msg => {
     if (msg.author.id === client.user.id) {
         return;
     }
-    if(msg.content === '.test'){
+    if(msg.content === '.ピーチは抜ける'){
         let peach = Math.floor( Math.random() * 191 ) ;
         msg.channel.send({ files: ['./peachimages/peach ('+peach+').jpg'] })
     }
