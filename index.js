ae07f0167a361a24992eff1a34b860be7b4c82d2
nbatotu <nbatotu+ac@gmail.com>
2020-08-13 21:19:05 +0900

    AWE

--- index.js
+++ index.js
@@ -37,11 +37,12 @@ client.on('message', msg => {
     if (msg.content.startsWith('.play') && msg.guild) {
         let channel = msg.member.voice.channel
         let sound = msg.content.slice(6)
+        let mp3 = 'sound/'+msg.content.slice(6)+'.mp3'
         // コマンドを実行したメンバーがボイスチャンネルに入ってなければ処理を止める
         if (!channel) return msg.reply('先にボイスチャンネルに参加してください。')
         channel.join().then(connection => {
-            connection.play('sound/'+sound+'.mp3')
             msg.reply(sound+'を再生します')
+            connection.play(mp3)
         });
     } 
     
